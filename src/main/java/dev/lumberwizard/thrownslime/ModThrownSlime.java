969526197140c225f69be349b2897d05694c40e0
Yotam Gur <yotamgur06@gmail.com>
2019-02-07 19:06:42 +0200

    Maybe I'd gone a little overboard with that 2.0 thing

--- src/main/java/dev/lumberwizard/thrownslime/ModThrownSlime.java
+++ src/main/java/dev/lumberwizard/thrownslime/ModThrownSlime.java
@@ -27,7 +27,7 @@ public class ModThrownSlime {
 
 	public static final String MOD_NAME = "Thrown Slime";
 	public static final String MODID = "thrownslime";
-	public static final String VERSION = "2.0.0";
+	public static final String VERSION = "1.3.0";
 
 	public static Logger logger;
 
