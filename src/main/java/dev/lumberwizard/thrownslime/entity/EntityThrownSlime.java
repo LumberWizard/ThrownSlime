fc267ca640c70c5858d927448b07711436f76ac1
Yotam Gur <yotamgur06@gmail.com>
2019-02-10 23:48:27 +0200

    I know how to do things now

--- src/main/java/dev/lumberwizard/thrownslime/entity/EntityThrownSlime.java
+++ src/main/java/dev/lumberwizard/thrownslime/entity/EntityThrownSlime.java
@@ -2,7 +2,9 @@ package dev.lumberwizard.thrownslime.entity;
 
 import dev.lumberwizard.thrownslime.ModConfig;
 import dev.lumberwizard.thrownslime.ModThrownSlime;
+import io.netty.buffer.ByteBuf;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityList;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.monster.EntitySlime;
 import net.minecraft.entity.monster.EntitySnowman;
@@ -12,109 +14,119 @@ import net.minecraft.init.Items;
 import net.minecraft.init.MobEffects;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.network.datasync.DataParameter;
-import net.minecraft.network.datasync.DataSerializers;
-import net.minecraft.network.datasync.EntityDataManager;
 import net.minecraft.potion.PotionEffect;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.EnumParticleTypes;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.common.Loader;
+import net.minecraftforge.fml.common.network.ByteBufUtils;
+import net.minecraftforge.fml.common.registry.IEntityAdditionalSpawnData;
+import net.minecraftforge.fml.common.registry.IThrowableEntity;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
-public class EntityThrownSlime extends EntitySnowball {
+public class EntityThrownSlime extends EntitySnowball implements IEntityAdditionalSpawnData, IThrowableEntity {
 
-	private static final DataParameter<Integer> ITEM = EntityDataManager.<Integer>createKey(EntityThrownSlime.class,
-			DataSerializers.VARINT);
+	private ItemStack slimeball;
+	private int age;
 
 	public EntityThrownSlime(World world) {
 		super(world);
-		this.dataManager.register(ITEM, Item.getIdFromItem(Items.SLIME_BALL) << 4);
+		slimeball = new ItemStack(Items.SLIME_BALL);
+		age = 0;
 	}
 
 	public EntityThrownSlime(World worldIn, EntityLivingBase throwerIn, ItemStack item) {
 		super(worldIn, throwerIn);
-		this.dataManager.register(ITEM, Item.getIdFromItem(item.getItem()) << 4 | item.getMetadata());
+		slimeball = item;
+		age = 0;
 	}
 
-	public EntityThrownSlime(World worldIn, double x, double y, double z, ItemStack item)
-    {
-        super(worldIn, x, y, z);
-		this.dataManager.register(ITEM, Item.getIdFromItem(item.getItem()) << 4 | item.getMetadata());
-    }
+	public EntityThrownSlime(World worldIn, double x, double y, double z, ItemStack item) {
+		super(worldIn, x, y, z);
+		slimeball = item;
+		age = 0;
+	}
+
+	@Override
+	public void onUpdate() {
+		super.onUpdate();
+		age++;
+	}
 
 	public ItemStack getSlimeball() {
-		return new ItemStack(Item.getItemById(dataManager.get(ITEM) >>> 4), 1, dataManager.get(ITEM) & 0b1111);
+		return slimeball;
 	}
 
-	public void writeEntityToNBT(NBTTagCompound compound) {
-		super.writeEntityToNBT(compound);
-		compound.setInteger("item", dataManager.get(ITEM));
-	}
-
-	public void readEntityFromNBT(NBTTagCompound compound) {
-        super.readEntityFromNBT(compound);
-		dataManager.register(ITEM, compound.getInteger("item"));
-	}
-    
-    @Override
-    @SideOnly(Side.CLIENT)
-    public void handleStatusUpdate(byte id)
-    {
-        if (id == 3)
-        {
-            for (int i = 0; i < 8; ++i)
-            {
-                world.spawnParticle(EnumParticleTypes.SLIME, posX, posY, posZ, 0.0D, 0.0D, 0.0D);
-            }
-        }
-    }
-    
-    @Override
-    protected void onImpact(RayTraceResult result)
-    {
-        Entity entity = result.entityHit;
-        
-		if (entity != null && !(entity instanceof EntitySlime)) {
-			int i = 0;
-			if (!world.isRemote && entity instanceof EntityLivingBase && this.ticksExisted > 0
-					&& (!(entity instanceof EntityPlayer) || ModConfig.affectCreative
-							|| !((EntityPlayer) entity).capabilities.isCreativeMode)) {
-				((EntityLivingBase) entity).addPotionEffect(
-						new PotionEffect(MobEffects.SLOWNESS, ModConfig.effectTime * 20, ModConfig.level - 1));
-				Item item = getSlimeball().getItem();
-				if (item == Items.MAGMA_CREAM) {
-					entity.setFire(ModConfig.effectTime * 20);
-					if (entity instanceof EntitySnowman) {
-						i = 3;
-					}
-				}
-				if (Loader.isModLoaded("industrialforegoing") && item == ModThrownSlime.PINK_SLIME) {
-					((EntityLivingBase) entity).addPotionEffect(
-							new PotionEffect(MobEffects.GLOWING, ModConfig.effectTime * 20, ModConfig.level - 1));
-				}
-				entity.attackEntityFrom(DamageSource.causeThrownDamage(this, this.getThrower()), i);
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void handleStatusUpdate(byte id) {
+		if (id == 3) {
+			for (int i = 0; i < 8; ++i) {
+				world.spawnParticle(EnumParticleTypes.SLIME, posX, posY, posZ, 0.0D, 0.0D, 0.0D);
 			}
 		}
+	}
 
-		if (entity != null && !(entity instanceof EntitySlime))
-        {
-			entity.attackEntityFrom(DamageSource.causeThrownDamage(this, this.getThrower()), 0);
-			if (entity instanceof EntityLivingBase && !world.isRemote
-					&& (this.getThrower() == null || !this.getThrower().isEntityEqual(entity))
-					&& (!(entity instanceof EntityPlayer) || ModConfig.affectCreative
-							|| !((EntityPlayer) entity).capabilities.isCreativeMode)) {
+	@Override
+	protected void onImpact(RayTraceResult result) {
+		Entity hitEntity = result.entityHit;
+		if (age <= 2 && hitEntity.isEntityEqual(getThrower())) return;
+		if (hitEntity == null) {
+			if (!world.isRemote) {
+				this.world.setEntityState(this, (byte) 3);
+				this.setDead();
 			}
-        }
+			return;
+		}
+		Item slimeballItem = getSlimeball().getItem();
+		if (!world.isRemote) {
+			if ((ModConfig.affectCreative
+					|| !(hitEntity instanceof EntityPlayer && ((EntityPlayer) hitEntity).capabilities.isCreativeMode))) {
+				if (slimeballItem == Items.SLIME_BALL && hitEntity instanceof EntityLivingBase
+						&& !(hitEntity instanceof EntitySlime)) {
+					((EntityLivingBase) hitEntity).addPotionEffect(
+							new PotionEffect(MobEffects.SLOWNESS, ModConfig.effectTime * 20, ModConfig.level - 1));
+				} else if (slimeballItem == Items.MAGMA_CREAM && !hitEntity.isImmuneToFire()) {
+					hitEntity.setFire(ModConfig.effectTime);
+				} else if (Loader.isModLoaded("industrialforegoing") && slimeballItem == ModThrownSlime.PINK_SLIME
+						&& hitEntity instanceof EntityLivingBase && !EntityList.isMatchingName(hitEntity,
+								new ResourceLocation("industrialforegoing", "pink_slime"))) {
+					((EntityLivingBase) hitEntity).addPotionEffect(
+							new PotionEffect(MobEffects.GLOWING, ModConfig.effectTime * 20, ModConfig.level - 1));
+				}
+			}
 
-        if (!this.world.isRemote)
-        {
-            this.world.setEntityState(this, (byte)3);
-            this.setDead();
-        }
-    }
+			this.world.setEntityState(this, (byte) 3);
+			this.setDead();
+		}
+		if (!(slimeballItem == Items.MAGMA_CREAM && hitEntity.isImmuneToFire())
+				&& !(slimeballItem == Items.SLIME_BALL && hitEntity instanceof EntitySlime)
+				&& !(Loader.isModLoaded("industrialforegoing") && slimeballItem == ModThrownSlime.PINK_SLIME
+						&& EntityList.isMatchingName(hitEntity,
+								new ResourceLocation("industrialforegoing", "pink_slime"))))
+			hitEntity.attackEntityFrom(DamageSource.causeThrownDamage(this, this.getThrower()),
+					slimeballItem == Items.MAGMA_CREAM && hitEntity instanceof EntitySnowman ? 3 : 0);
+	}
+
+	@Override
+	public void setThrower(Entity entity) {
+		if (entity instanceof EntityLivingBase)
+			this.thrower = (EntityLivingBase) entity;
+		else
+			thrower = null;
+	}
+
+	@Override
+	public void writeSpawnData(ByteBuf buffer) {
+		ByteBufUtils.writeItemStack(buffer, slimeball);
+	}
+
+	@Override
+	public void readSpawnData(ByteBuf additionalData) {
+		slimeball = ByteBufUtils.readItemStack(additionalData);
+	}
 
 }
